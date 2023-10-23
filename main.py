b35cff05ebc976f6035c958c0357901e47779d48
David Gomez <david@numero1.co>
2023-10-22 20:49:08 -0500

    add post methos for recived data

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Data(BaseModel):
+    name: str
+    password: str
+
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+@app.post("/towerdata/")
+async def say_hello(data: Data):
+    print(data)
+    return data
