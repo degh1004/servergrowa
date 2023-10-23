16b67208474c20e84022ecb9a14c496298234638
degh1004 <david@se360.co>
2023-10-22 21:01:45 -0500

    fix main

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
-
-origins = [
-    "*"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 class Data(BaseModel):
     name: str
